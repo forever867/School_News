d24ca99ac272c0115c1228a8313ea7215276c64c
forever867 <2648688410@qq.com>
2026-04-20 16:07:37 +0800

    第二次提交，后端

---
+++ src/test/java/com/cared/CaredApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cared;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaredApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
